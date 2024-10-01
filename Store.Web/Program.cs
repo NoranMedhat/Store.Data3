f1e24d57e7b7d8a6a25ff5e50c898d07767a332b
Noran <Noranmedht63@gmail.com>
2024-10-01 04:31:58 +0300

    Add project files.

---
+++ Store.Web/Program.cs
@@ -0,0 +1,65 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using Store.Data.Contexts;
+using Store.Repository;
+using Store.Repository.Interfaces;
+using Store.Repository.Repositories;
+using Store.Service.HandleResponses;
+using Store.Service.Services.ProductService;
+using Store.Service.Services.ProductService.Dto;
+using Store.Web.Extentions;
+using Store.Web.Helper;
+using Store.Web.Middleware;
+
+namespace Store.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+            {
+                var configurations = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(configurations);
+            });
+            builder.Services.AddApplicationServices();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            await ApplySeeding.ApplySeedingAsync(app);
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
